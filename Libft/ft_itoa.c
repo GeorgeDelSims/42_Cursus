5babe9e69bbb5ccdf811f00d3fbab085ddad3dfc
George Sims <gsims@c1r11s8.42lausanne.ch>
2023-10-10 19:18:23 +0200

    itoa too many lines

--- Libft/ft_itoa.c
+++ Libft/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: gsims <marvin@42lausanne.ch>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 17:22:50 by gsims             #+#    #+#             */
-/*   Updated: 2023/10/10 18:37:56 by gsims            ###   ########.fr       */
+/*   Updated: 2023/10/10 19:17:58 by gsims            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,44 +17,49 @@ int	ft_get_size(int n)
 	int	size;
 
 	size = 1;
-	while (n /= 10)
+	while (n != 0)
+	{
+		n /= 10;
 		size++;
+	}
+	size--;
 	return (size);
 }
 
 char	*ft_itoa(int n)
 {
-	int		size;
-	int		i;
-	int		sign;
-	char	*res;
+	int			size;
+	long int	num;
+	char		*res;
 
-	sign = 0;
+	num = n;
 	size = ft_get_size(n);
 	if (n == -2147483648)
 		return (ft_strdup("-2147483648"));
 	if (n < 0)
 	{
 		size++;
-		n *= -1;
-		sign = 1;
+		num *= -1;
 	}
 	res = (char *)malloc(size + 1);
 	if (res == NULL)
-		return NULL;
-	if (n == 0)
-		res[0] = '0';
-	i = 0;
-	while (n /= 10)
-		res[i++] = (n % 10) + '0';
-	printf("%d\n", n);
+		return (NULL);
+	while (size--)
+	{
+		*(res + size) = (num % 10) + '0';
+		num = num / 10;
+	}
+	if (n < 0)
+		*res = '-';
+	else if (n == 0)
+		*res = '0';
 	return (res);
 }
-
+/*
 int	main()
 {
-	int	n = 67550;
+	int	n = -52405430;
 
 	printf("%s\n", ft_itoa(n));
 	return (0);
-}
+}*/
