27a745ebc678c747e6a22c518a4b24de4252f9ea
George Sims <gsims@c1r11s6.42lausanne.ch>
2023-10-25 16:33:01 +0200

    getnextline started

--- get_next_line_home/get_next_line.h
+++ get_next_line_home/get_next_line.h
@@ -1,37 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gsims <gsims@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/25 14:20:16 by gsims             #+#    #+#             */
-/*   Updated: 2023/10/25 16:32:32 by gsims            ###   ########.fr       */
+/*   Created: 2023/10/25 14:20:13 by gsims             #+#    #+#             */
+/*   Updated: 2023/10/25 16:25:00 by gsims            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
-//function reads a line from file at given file descriptor
+# include <unistd.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE (42)
+
 char    *get_next_line(int fd)
-{
-    static char *stash;
-    int         buf;
-    
-    buf = BUFFER_SIZE;
-    
-    return ("getnextline");
-}
 
-int main(int argc, char *argv[])
-{
-    if (argc < 2)
-    {
-        write(1, "More arguments needed\0", 22);
-        return (NULL);
-    }
-    else 
-    {
-        
-    }
-}
+#endif
--- get_next_line_home/get_next_line_utils.c
+++ get_next_line_home/get_next_line_utils.c
@@ -1,37 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gsims <gsims@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/25 14:20:16 by gsims             #+#    #+#             */
-/*   Updated: 2023/10/25 16:32:32 by gsims            ###   ########.fr       */
+/*   Created: 2023/10/25 14:27:24 by gsims             #+#    #+#             */
+/*   Updated: 2023/10/25 14:27:25 by gsims            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
-
-//function reads a line from file at given file descriptor
-char    *get_next_line(int fd)
-{
-    static char *stash;
-    int         buf;
-    
-    buf = BUFFER_SIZE;
-    
-    return ("getnextline");
-}
-
-int main(int argc, char *argv[])
-{
-    if (argc < 2)
-    {
-        write(1, "More arguments needed\0", 22);
-        return (NULL);
-    }
-    else 
-    {
-        
-    }
-}
