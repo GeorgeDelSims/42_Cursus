ede4252a5669f464e0c9bed30d210f069659e42e
George Sims <gsims@c1r11s6.42lausanne.ch>
2023-11-14 18:11:39 +0100

    started pipex

---
+++ pipex_home/main.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsims <marvin@42lausanne.ch>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 15:04:53 by gsims             #+#    #+#             */
+/*   Updated: 2023/11/14 17:41:31 by gsims            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+// Main Function takes all four args as input 
+// arg[1] and arg[4] are file names
+// arg[2] and arg[3] are shell commands.
+// the result of cmd1 being applied to file1 needs to be fed to cmd2 and applied to file2.
+
+int	main(int argc, char *argv[])
+{
+	if (argc != 5)
+		return (NULL);
+	
+}
