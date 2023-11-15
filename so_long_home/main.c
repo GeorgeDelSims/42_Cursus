56b48dcdf2a34be44b34eb69069a3bf77e275a32
George Sims <gsims@c1r11s6.42lausanne.ch>
2023-11-15 16:07:02 +0100

    so_long moving slowly forward

--- so_long_home/main.c
+++ so_long_home/main.c
@@ -6,21 +6,37 @@
 /*   By: gsims <gsims@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 09:31:12 by gsims             #+#    #+#             */
-/*   Updated: 2023/11/15 12:01:19 by gsims            ###   ########.fr       */
+/*   Updated: 2023/11/15 16:05:47 by gsims            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void	*mlx_init();
-
-int	main()
-{
+typedef struct	s_vars {
 	void	*mlx;
-	void	*mlx_win;
+	void	*win;
+	int		keycode;
+}				t_vars;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1080, 1080, "so_long");
-	mlx_loop(mlx);
+int ft_key_hook(int keycode, t_vars *vars)
+{
+	printf("%d\n", keycode);
 	return (0);
+}
+
+int	mouse_hook(int keycode, t_vars *vars)
+{
+	printf("%d\n", keycode);
+	return (0);
+}
+
+int main()
+{
+	t_vars	vars;
+
+	vars.mlx = mlx_init();
+	vars.win = mlx_new_window(vars.mlx, 1080, 1080, "so_long");
+	mlx_key_hook(vars.win, ft_key_hook, &vars);
+	mlx_mouse_hook(vars.win, mouse_hook, &vars);
+	mlx_loop(vars.mlx);
 }
