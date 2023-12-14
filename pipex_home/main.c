d88794642578d785d9aac491dcdc0dd7846acd94
George Sims <georgesims@Georges-MacBook-Pro.local>
2023-12-14 09:47:52 +0100

    small changes

--- pipex_home/main.c
+++ pipex_home/main.c
@@ -6,10 +6,9 @@
 /*   By: georgesims <georgesims@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 15:04:53 by gsims             #+#    #+#             */
-/*   Updated: 2023/12/13 08:41:07 by georgesims       ###   ########.fr       */
+/*   Updated: 2023/12/14 09:14:33 by georgesims       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 // Main Function takes all four args as input 
 // arg[1] and arg[4] are file names
@@ -18,26 +17,50 @@
 
 #include "pipex.h"
 
-int	main(int argc, char *argv[])
+void	child_process(t_data *data, char *argv[], char **env)
 {
-	int		file1;
-	int		file2;
-	int		fd[2];
+	// close the read end of the pipe
+	close(data->fd[0]);
+	// make the standard output (stdout = 1) of the current process
+	// into a copy of the write end of the pipe (fd[1])
+	dup2(data->fd[1], 1);
+	// close the write end of the pipe
+	close(data->fd[1]);
+	// execute the first command
+	execve(argv[2], argv[2], env);
+}
+
+void	parent_process(t_data *data, char *argv[], char **env)
+{
+	// close the write end of the pipe
+	close(data->fd[1]);
+	// make the standard input (stdin = 0) of the current process
+	// into a copy of the read end of the pipe (fd[0])
+	dup2(data->fd[0], 0);
+	// close the read end of the pipe
+	close(data->fd[0]);
+	// execute the second command
+	execve(argv[3], argv[3], env);
+}
+
+int	main(int argc, char *argv[], char **env)
+{
 	pid_t	pid;
+	t_data	*data;
 	
 	if (argc != 5)
 		return (0);
 	// open file1 for reading and file2 for writing
-	file1 = open(argv[1], O_RDONLY);
-	file2 = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (file1 == -1 || file2 == -1)
+	data->file1 = open(argv[1], O_RDONLY);
+	data->file2 = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (data->file1 == -1 || data->file2 == -1)
 	{
 		perror("Error");
 		exit(1);
 	}
 	// pipe reads from fd[0] and writes to fd[1]
-	pipe(fd);
-	if (pipe(fd) == -1)
+	pipe(data->fd);
+	if (pipe(data->fd) == -1)
 	{
 		perror("Error");
 		exit(1);
@@ -51,28 +74,9 @@ int	main(int argc, char *argv[])
 	}
 	// child process
 	if (pid == 0)
-	{
-		// close the read end of the pipe
-		close(fd[0]);
-		// make the standard output (stdout = 1) of the current process
-		// into a copy of the write end of the pipe (fd[1])
-		dup2(fd[1], 1);
-		// close the write end of the pipe
-		close(fd[1]);
-		// execute the first command
-		execve(argv[2], argv[2], NULL);
-	}
-	// parent process
+		child_process(data->fd, argv, env);
+	// parent process	
 	else
-	{
-		// close the write end of the pipe
-		close(fd[1]);
-		// make the standard input (stdin = 0) of the current process
-		// into a copy of the read end of the pipe (fd[0])
-		dup2(fd[0], 0);
-		// close the read end of the pipe
-		close(fd[0]);
-		// execute the second command
-		execve(argv[3], argv[3], NULL);
-	}
+		parent_process(data->fd, argv, env);
+	return (0);
 }
--- pipex_home/pipex.h
+++ pipex_home/pipex.h
@@ -6,7 +6,7 @@
 /*   By: georgesims <georgesims@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 17:43:38 by gsims             #+#    #+#             */
-/*   Updated: 2023/12/13 08:05:09 by georgesims       ###   ########.fr       */
+/*   Updated: 2023/12/14 09:15:11 by georgesims       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,17 @@
 # include <fcntl.h>
 # include <stdio.h>
 #include <sys/types.h>
+
+typedef struct  s_data
+{
+    int    file1;
+    int    file2;
+    int    fd[2];    
+}            t_data;
+
+void	child_process(t_data *data, char *argv[], char **env);
+void	parent_process(t_data *data, char *argv[], char **env);
+int 	main(int argc, char *argv[], char **env);
 
 
 #endif
